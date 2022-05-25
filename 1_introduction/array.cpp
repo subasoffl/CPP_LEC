c8334048cb54e0f2856905ad08a63d65c775b93e
subas kandel <69797726+subasoffl@users.noreply.github.com>
2022-05-25 19:31:35 +0545

    use of array in C++

---
+++ 1_introduction/array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int arr[5]={9,8,5,3,10};
+    // printing the array
+    cout <<"Array of index 0 is\t"<<arr[0]<<endl;
+    cout <<"Array of index 1 is\t"<<arr[1]<<endl;
+    cout <<"Array of index 2 is\t"<<arr[2]<<endl;
+    cout <<"Array of index 3 is\t"<<arr[3]<<endl;
+    cout <<"Array of index 4 is\t"<<arr[4]<<endl;
+   
+   return 0;
+}
